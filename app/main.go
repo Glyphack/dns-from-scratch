79d90263b602cf2f70bdf85a0136fd4f55cd7d7c
Glyphack <sh.hooshyari@gmail.com>
2024-09-17 20:54:40 +0200

    Stage 3

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 	"net"
@@ -38,11 +39,43 @@ func main() {
 		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
 
 		// Create an empty response
-		response := make([]byte, 12)
+		response := []byte{}
 
-		binary.BigEndian.PutUint16(response[0:2], 1234)
+		// Header
+		header := make([]byte, 12)
+		// id
+		binary.BigEndian.PutUint16(header[0:2], 1234)
 
-		response[2] |= 0b10000000
+		// qr opcode aa tc rd
+		header[2] |= 0b10000000
+
+		// ra z rcode
+		header[3] = 0b00000000
+
+		// question count
+		header[4] = 0b00000000
+		header[5] = 0b00000001
+		response = append(response, header...)
+
+		// Question
+		question := []byte{}
+		domain := "codecrafters.io"
+		labels := bytes.Split([]byte(domain), []byte("."))
+		fmt.Printf("labels: %v\n", labels)
+
+		for label := range labels {
+			question = append(question, byte(len(labels[label])))
+			question = append(question, labels[label]...)
+		}
+		question = append(question, 0)
+
+		// question - type
+		question = binary.BigEndian.AppendUint16(question, 1)
+		// question - class
+		question = binary.BigEndian.AppendUint16(question, 1)
+
+		fmt.Printf("question: %v\n", question)
+		response = append(response, question...)
 
 		_, err = udpConn.WriteToUDP(response, source)
 		if err != nil {
